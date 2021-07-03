8113e421340cb9067e8c569fb34b9482ed2291a8
Claudio Rigollet Meléndez <claudiorigo@hotmail.com>
2021-07-03 04:40:59 -0400

    Agregar base de api

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from core.serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+
+# Create your views here.
