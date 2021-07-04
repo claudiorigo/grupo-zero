982b92a7214e903d40a87a9155d607169bf9a239
Claudio Rigollet Meléndez <claudiorigo@hotmail.com>
2021-07-04 11:04:56 -0400

    Finalizado listo para presentar video

--- core/static/core/js/api.js
+++ core/static/core/js/api.js
@@ -4,6 +4,7 @@ const cargarCategoria = async function(){
     let res = await axios.get("../../static/core/api/categoria.json");    
     let cards = res.data.arrayCards;
 
+    //const fecha = document.querySelector("#fecha");
     const contenedor = document.querySelector("#contenedor");
     const molde = document.querySelector(".molde-card"); 
 
@@ -13,6 +14,7 @@ const cargarCategoria = async function(){
         copia.querySelector(".imagen-card").src = p.imagen;
         copia.querySelector(".categoria-card").innerText = p.categoria;        
         copia.querySelector(".descripcion-card").innerText = p.descripcion
+        //copia.querySelector("#fecha").src = p.imagen;
         copia.querySelector(".link-card").addEventListener("click", function(){
             location.href = p.link;            
         });
